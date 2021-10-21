5a0a67b536208143f91a87150b9f96524459a66c
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-20 17:35:05 -0700

    TDD findById

--- Data/storage-utils.js
+++ Data/storage-utils.js
@@ -1,3 +1,13 @@
+// import pokemon from './pokemon.js';
+
+export function findById(id, pokemon) {
+    for (let pokes of pokemon) {
+        if (pokes.id === id) {
+            return pokes;
+        }
+    }
+}
+
 export function getPokedex(){
 
     const dexString = localStorage.getItem('POKEDEX') || '[]';
@@ -5,4 +15,17 @@ export function getPokedex(){
     const pokedex = JSON.parse(dexString);
 
     return pokedex;
+}
+
+export function setPokedex(id){
+    const pokedex = getPokedex();
+    const pokeItem = findById(id, pokedex);
+    if (pokeItem) {
+        pokeItem.qty++;
+    } else {
+        const newItem = { id: id, qty: 1 };
+        pokedex.push(newItem);
+    }
+    const dexString = JSON.stringify(pokedex);
+    localStorage.setItem('POKEDEX', dexString);
 }
--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 // import pokemon from '../data/pokemon.js';
-import { getPokedex } from '../data/storage-utils.js';
+import pokemon from '../data/pokemon.js';
+import { getPokedex, setPokedex, findById } from '../data/storage-utils.js';
 
 const test = QUnit.test;
 
@@ -18,6 +19,51 @@ const test = QUnit.test;
 //     // Make assertions about what is expected versus the actual result
 //     expect.equal(actual, expected);
 // });
+test('findById should return the pokemon matching the ID', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = {
+        '_id':'5cef3501ef6005a77cd4fd17',
+        'pokemon':'bulbasaur',
+        'id':1,
+        'species_id':1,
+        'height':7,
+        'weight':69,
+        'base_experience':64,
+        'type_1':'grass',
+        'type_2':'poison',
+        'attack':49,
+        'defense':49,
+        'hp':45,
+        'special_attack':65,
+        'special_defense':65,
+        'speed':45,
+        'ability_1':'overgrow',
+        'ability_2':'NA',
+        'ability_hidden':'chlorophyll',
+        'color_1':'#78C850',
+        'color_2':'#A040A0',
+        'color_f':'#81A763',
+        'egg_group_1':'monster',
+        'egg_group_2':'plant',
+        'url_image':'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+        'generation_id':1,
+        'evolves_from_species_id':'NA',
+        'evolution_chain_id':1,
+        'shape_id':8,
+        'shape':'quadruped',
+        'pokebase':'bulbasaur',
+        'pokedex':'http://www.pokemon.com/us/pokedex/bulbasaur'
+    };
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = findById(1, pokemon);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
+});
+
 
 test('getPokedex should return the pokedex if it exists', (expect)=>{
     //arrange
@@ -29,4 +75,22 @@ test('getPokedex should return the pokedex if it exists', (expect)=>{
     const pokedex = getPokedex();
     //assert
     expect.deepEqual(pokedex, fakeDex);
+});
+
+test('setPokedex should increment the quantity of the item in the id', (expect)=>{
+    // arrange
+    const fakeDex = [
+        { id: '1', qty: 1 },
+        { id: '2', qty: 2 }
+    ];
+    localStorage.setItem('CART', JSON.stringify(fakeDex));
+    // act
+    setPokedex('1');
+    const pokedex = getPokedex();
+    const expected = [
+        { id: '1', qty: 2 },
+        { id: '2', qty: 2 }
+    ];
+    // assert
+    expect.deepEqual(pokedex, expected);
 });
