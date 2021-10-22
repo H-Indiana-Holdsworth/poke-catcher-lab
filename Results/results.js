3749de03dd71172306ccdfcb14e6050173af5352
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-22 14:04:00 -0700

    adds chart

--- Results/results.js
+++ Results/results.js
@@ -16,7 +16,7 @@ for (let poke of pokedex){
     img.src = pokemonItem.url_image;
 
     const header = document.createElement('header');
-    header.textContent = pokemonItem.name;
+    header.textContent = pokemonItem.pokemon;
 
     const encounterDisplay = document.createElement('span');
     encounterDisplay.textContent = `Encountered: ${poke.encounter} `;
@@ -32,4 +32,48 @@ for (let poke of pokedex){
 
 homepage.addEventListener('click', () => {
     localStorage.removeItem('POKEDEX');
+});
+
+const names = pokedex.map((poke)=>{
+    const pokedex = findById(poke.id, pokemon);
+    return pokedex.pokemon;
+});
+
+console.log(names);
+const capture = pokedex.map(pokemon=>pokemon.capture);
+var ctx = document.getElementById('pokedexChart').getContext('2d');
+//eslint-disable-next-line no-undef
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: names,
+        datasets: [{
+            label: '# of Times Captured',
+            data: capture,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
